cbb02001867637b7e823182f7bc64cbb54569f04
julien.viet@gmail.com <julien.viet@gmail.com@e2aba3f2-7a3a-11de-96f2-af6933d8d261>
2009-11-14 09:34:41 +0000

    add unit tests for various visiblity access for properties (package protected and protected)

---
+++ core/src/test/java/org/chromattic/test/methodinvocation/visibility/VisibilityTestCase.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2003-2009 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.chromattic.test.methodinvocation.visibility;
+
+import org.chromattic.core.DomainSession;
+import org.chromattic.test.AbstractTestCase;
+
+/**
+ * @author <a href="mailto:julien.viet@exoplatform.com">Julien Viet</a>
+ * @version $Revision$
+ */
+public class VisibilityTestCase extends AbstractTestCase {
+
+  protected void createDomain() {
+    addClass(A.class);
+  }
+
+  public void testProtectedProperty() throws Exception {
+    DomainSession session = login();
+    A a = session.insert(A.class, "a");
+    assertNull(a.getProtectedProperty());
+    a.setProtectedProperty("foo");
+    assertEquals("foo", a.getProtectedProperty());
+  }
+
+  public void testPackageProtectedProperty() throws Exception {
+    DomainSession session = login();
+    A a = session.insert(A.class, "a");
+    assertNull(a.getPackageProtectedProperty());
+    a.setPackageProtectedProperty("bar");
+    assertEquals("bar", a.getPackageProtectedProperty());
+  }
+}
