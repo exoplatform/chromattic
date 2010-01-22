9cd558dd69b7162b3a684bb88dcef7ac845a783a
julien.viet <julien.viet@e2aba3f2-7a3a-11de-96f2-af6933d8d261>
2010-01-22 23:51:14 +0000

    update for JCR based on JBoss Cache

---
+++ exo/src/test/java/org/chromattic/exo/RepositoryBootstrapTestCase.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2003-2009 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.chromattic.exo;
+
+import junit.framework.TestCase;
+
+import javax.jcr.Repository;
+import javax.jcr.Session;
+
+/**
+ * @author <a href="mailto:julien.viet@exoplatform.com">Julien Viet</a>
+ * @version $Revision$
+ */
+public class RepositoryBootstrapTestCase extends TestCase {
+
+  public void testBootstrap() throws Exception {
+
+    RepositoryBootstrap bootstrap = new RepositoryBootstrap();
+    bootstrap.bootstrap();
+    Repository repo = bootstrap.getRepository();
+    Session session = repo.login();
+    session.getRootNode().addNode("foo", "nt:base");
+    session.save();
+    session.logout();
+  }
+}
