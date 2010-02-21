cf6d8f2d8afc5c0707c1214f24ad29ef86d7d61b
julien.viet <julien.viet@e2aba3f2-7a3a-11de-96f2-af6933d8d261>
2010-02-21 15:07:51 +0000

    actually forgot that

---
+++ metamodel/src/main/java/org/chromattic/metamodel/typegen/Property.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2003-2009 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.chromattic.metamodel.typegen;
+
+import org.chromattic.metamodel.bean.MultiValuedPropertyInfo;
+import org.chromattic.metamodel.bean.PropertyInfo;
+import org.chromattic.metamodel.bean.SimpleType;
+import org.chromattic.metamodel.bean.SimpleValueInfo;
+import org.chromattic.metamodel.mapping.jcr.JCRPropertyMapping;
+
+import javax.jcr.PropertyType;
+
+/**
+ * @author <a href="mailto:julien.viet@exoplatform.com">Julien Viet</a>
+ * @version $Revision$
+ */
+class Property {
+
+  Property(String name, boolean multiple, int type) {
+    this.name = name;
+    this.multiple = multiple;
+    this.type = type;
+  }
+
+  Property(JCRPropertyMapping mapping, PropertyInfo<SimpleValueInfo> propertyInfo) {
+
+    int propertyType;
+    SimpleValueInfo simpleValueInfo = propertyInfo.getValue();
+    SimpleType stk = simpleValueInfo.getSimpleType();
+    if (stk == SimpleType.STRING) {
+      propertyType = PropertyType.STRING;
+    } else if (stk == SimpleType.LONG || stk ==SimpleType.PRIMITIVE_LONG) {
+      propertyType = PropertyType.LONG;
+    } else if (stk == SimpleType.PATH) {
+      propertyType = PropertyType.PATH;
+    } else if (stk == SimpleType.DATE) {
+      propertyType = PropertyType.DATE;
+    } else if (stk == SimpleType.BOOLEAN || stk ==SimpleType.PRIMITIVE_BOOLEAN) {
+      propertyType = PropertyType.BOOLEAN;
+    } else if (stk == SimpleType.INTEGER || stk ==SimpleType.PRIMITIVE_INTEGER) {
+      propertyType = PropertyType.LONG;
+    } else if (stk == SimpleType.FLOAT || stk ==SimpleType.PRIMITIVE_FLOAT) {
+      propertyType = PropertyType.DOUBLE;
+    } else if (stk == SimpleType.DOUBLE || stk ==SimpleType.PRIMITIVE_DOUBLE) {
+      propertyType = PropertyType.DOUBLE;
+    } else if (stk == SimpleType.STREAM) {
+      propertyType = PropertyType.BINARY;
+    } else if (stk instanceof SimpleType.Enumerated) {
+      propertyType = PropertyType.STRING;
+    } else {
+      throw new AssertionError();
+    }
+
+    //
+    this.multiple = propertyInfo instanceof MultiValuedPropertyInfo;
+    this.name = mapping.getName();
+    this.type = propertyType;
+  }
+
+  final String name;
+
+  final boolean multiple;
+
+  final int type;
+
+}
