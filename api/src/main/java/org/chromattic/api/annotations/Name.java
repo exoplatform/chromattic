666f99a004b23d3c222026d2049c35f7cca57e25
julien.viet <julien.viet@e2aba3f2-7a3a-11de-96f2-af6933d8d261>
2010-04-13 12:00:22 +0000

    update javadonc for @Name annotation

--- api/src/main/java/org/chromattic/api/annotations/Name.java
+++ api/src/main/java/org/chromattic/api/annotations/Name.java
@@ -25,7 +25,16 @@ import java.lang.annotation.Target;
 import java.lang.annotation.ElementType;
 
 /**
- * Annotates a bean property getter to return the current name of the related jcr node.
+ * Annotates a bean property to map it against the current name of the related jcr node.
+ * <ul>
+ * <li>When the entity is {@link org.chromattic.api.Status#TRANSIENT} the name is saved along with the entity
+ * until it is persisted. When the entity is persisted, the name is used when no additional name is provided.</li>
+ * <li>When the entity is {@link org.chromattic.api.Status#PERSISTENT} the name is associated with the current JCR
+ * node name. A property read returns the current node name and a property write performs a JCR move operation to
+ * rename the entity with the new name.</li>
+ * <li>When the entity is {@link org.chromattic.api.Status#REMOVED} any property access throws an
+ * {@link IllegalStateException}.</li>
+ * </ul>
  *
  * @author <a href="mailto:julien.viet@exoplatform.com">Julien Viet</a>
  * @version $Revision$
