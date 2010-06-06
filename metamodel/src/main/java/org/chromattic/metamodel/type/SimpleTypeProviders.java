98183ab05a835069808d64bd8cf49083ded27e66
julien.viet <julien.viet@e2aba3f2-7a3a-11de-96f2-af6933d8d261>
2010-06-06 13:53:39 +0000

    hudson happy

--- metamodel/src/main/java/org/chromattic/metamodel/type/SimpleTypeProviders.java
+++ metamodel/src/main/java/org/chromattic/metamodel/type/SimpleTypeProviders.java
@@ -35,7 +35,7 @@ import java.util.Date;
  * @author <a href="mailto:julien.viet@exoplatform.com">Julien Viet</a>
  * @version $Revision$
  */
-class SimpleTypeProviders {
+public class SimpleTypeProviders {
 
   private SimpleTypeProviders() {
   }
