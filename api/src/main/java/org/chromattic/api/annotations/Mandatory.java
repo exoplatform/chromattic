25fc4bfcded4962dc1900693917243b80331a604
julien.viet <julien.viet@e2aba3f2-7a3a-11de-96f2-af6933d8d261>
2010-06-22 19:56:46 +0000

    minor

--- api/src/main/java/org/chromattic/api/annotations/Mandatory.java
+++ api/src/main/java/org/chromattic/api/annotations/Mandatory.java
@@ -27,6 +27,7 @@ import java.lang.annotation.Target;
 /**
  * @author <a href="mailto:julien.viet@exoplatform.com">Julien Viet</a>
  * @version $Revision$
+ * @since 1.1
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
