f9bdacfac466d8a1aca598fc49f021244a4235a4
julien.viet@gmail.com <julien.viet@gmail.com@e2aba3f2-7a3a-11de-96f2-af6933d8d261>
2011-03-23 22:00:23 +0000

    failing unit test that demonstrate improper scoping with mixins

---
+++ core/src/test/java/org/chromattic/test/format/X.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2003-2009 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.chromattic.test.format;
+
+import org.chromattic.api.RelationshipType;
+import org.chromattic.api.annotations.FormattedBy;
+import org.chromattic.api.annotations.OneToOne;
+import org.chromattic.api.annotations.Owner;
+import org.chromattic.api.annotations.PrimaryType;
+
+/**
+ * @author <a href="mailto:julien.viet@exoplatform.com">Julien Viet</a>
+ * @version $Revision$
+ */
+@PrimaryType(name = "format:x")
+@FormattedBy(FooPrefixerFormatter.class)
+public abstract class X {
+
+  @OneToOne(type = RelationshipType.EMBEDDED)
+  @Owner
+  public abstract Y getY();
+
+  public abstract void setY(Y y);
+}
