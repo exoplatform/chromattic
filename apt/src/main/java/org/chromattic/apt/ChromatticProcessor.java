74da5c2322e78b021d2a435f27b92fc816c5de73
Julien Viet <julien@julienviet.com>
2011-03-26 15:55:58 +0100

    handle wildcard and type array in a better way in APT plugin
    Conflicts:
    
    	ide/idea8/main.iws

--- apt/src/main/java/org/chromattic/apt/ChromatticProcessor.java
+++ apt/src/main/java/org/chromattic/apt/ChromatticProcessor.java
@@ -25,11 +25,7 @@ import org.reflext.api.ClassTypeInfo;
 import org.reflext.api.ClassIntrospector;
 import org.reflext.api.MethodInfo;
 import org.reflext.api.TypeInfo;
-import org.reflext.api.ParameterizedTypeInfo;
 import org.reflext.api.VoidTypeInfo;
-import org.reflext.api.TypeVariableInfo;
-import org.reflext.api.SimpleTypeInfo;
-import org.reflext.api.ArrayTypeInfo;
 import org.reflext.api.ClassKind;
 import org.reflext.core.TypeDomain;
 import org.reflext.apt.JavaxLangTypeModel;
@@ -77,7 +73,9 @@ public class ChromatticProcessor extends AbstractProcessor {
       try {
         JavaFileObject jfo = filer.createSourceFile(typeElt.getQualifiedName() + "_Chromattic", typeElt);
         PrintWriter out = new PrintWriter(jfo.openWriter());
-        writeClass(roundEnv, out, cti);
+        StringBuilder builder = new StringBuilder();
+        writeClass(roundEnv, builder, cti);
+        out.write(builder.toString());
         out.close();
       }
       catch (IOException e) {
@@ -90,10 +88,10 @@ public class ChromatticProcessor extends AbstractProcessor {
     return true;
   }
 
-  private void writeClass(RoundEnvironment roundEnv, PrintWriter out, ClassTypeInfo cti) {
+  private void writeClass(RoundEnvironment roundEnv, StringBuilder out, ClassTypeInfo cti) {
     String simpleClassName = cti.getSimpleName() + "_Chromattic";
-    out.write("package " + cti.getPackageName() + ";\n");
-    out.write("import " + Invoker.class.getName() + ";\n");
+    out.append("package ").append(cti.getPackageName()).append(";\n");
+    out.append("import ").append(Invoker.class.getName()).append(";\n");
 
     //
     StringBuffer sb = new StringBuffer("public class ");
@@ -110,18 +108,18 @@ public class ChromatticProcessor extends AbstractProcessor {
     sb.append(" {\n");
 
     //
-    out.write(sb.toString());
+    out.append(sb.toString());
 
     appendContructor(roundEnv, out, cti);
     appendAbstractMethods(simpleClassName, roundEnv, out, cti);
-    out.write("}\n");
+    out.append("}\n");
   }
 
-  private void appendContructor(RoundEnvironment roundEnv, PrintWriter out, ClassTypeInfo cti) {
-    out.println("public final " + MethodHandler.class.getName() + " handler;");
-    out.println("public " + cti.getSimpleName() + "_Chromattic(" + MethodHandler.class.getName() + " handler) {");
-    out.println("this.handler = handler;");
-    out.println("}");
+  private void appendContructor(RoundEnvironment roundEnv, StringBuilder out, ClassTypeInfo cti) {
+    out.append("public final ").append(MethodHandler.class.getName()).append(" handler;\n");
+    out.append("public ").append(cti.getSimpleName()).append("_Chromattic(").append(MethodHandler.class.getName()).append(" handler) {\n");
+    out.append("this.handler = handler;\n");
+    out.append("}\n");
   }
 
   private Iterable<MethodInfo> getMethodsToImplement(ClassTypeInfo cti) {
@@ -135,123 +133,7 @@ public class ChromatticProcessor extends AbstractProcessor {
     return methods;
   }
 
-  private String getCast(ClassTypeInfo ctx, TypeInfo ti) {
-    if (ti instanceof ClassTypeInfo) {
-      if (ti instanceof SimpleTypeInfo) {
-        switch (((SimpleTypeInfo)ti).getLiteralType()) {
-          case INT:
-            return "java.lang.Integer";
-          case BOOLEAN:
-            return "java.lang.Boolean";
-          case LONG:
-            return "java.lang.Long";
-          case DOUBLE:
-            return "java.lang.Double";
-          case FLOAT:
-            return "java.lang.Float";
-          default:
-            throw new UnsupportedOperationException();
-        }
-      }
-      else {
-        return ((ClassTypeInfo)ti).getName();
-      }
-    }
-    else if (ti instanceof ParameterizedTypeInfo) {
-      ParameterizedTypeInfo pti = (ParameterizedTypeInfo)ti;
-      TypeInfo rawType = pti.getRawType();
-      StringBuffer sb = new StringBuffer(getCast(ctx, rawType));
-      sb.append('<');
-      List<TypeInfo> arguments = pti.getTypeArguments();
-      for (int i = 0; i < arguments.size(); i++) {
-        TypeInfo argument = arguments.get(i);
-        if (i > 0) {
-          sb.append(',');
-        }
-        sb.append(getCast(ctx, argument));
-      }
-      sb.append('>');
-      return sb.toString();
-    }
-    else if (ti instanceof TypeVariableInfo) {
-      TypeVariableInfo tvi = (TypeVariableInfo)ti;
-      return getDeclaration(ctx, ctx.resolve(tvi));
-    }
-    else if (ti instanceof ArrayTypeInfo) {
-      ArrayTypeInfo ati = (ArrayTypeInfo)ti;
-      return getDeclaration(ctx, ctx.resolve(ati.getComponentType())) + "[]";
-    }
-    else {
-      throw new UnsupportedOperationException("Cannot handle declaration of " + ti);
-    }
-  }
-
-  private String getDeclaration(ClassTypeInfo ctx, TypeInfo ti) {
-    if (ti instanceof ClassTypeInfo) {
-      if (ti instanceof VoidTypeInfo) {
-        return "void";
-      }
-      else {
-        return ((ClassTypeInfo)ti).getName();
-      }
-    }
-    else if (ti instanceof ParameterizedTypeInfo) {
-      ParameterizedTypeInfo pti = (ParameterizedTypeInfo)ti;
-      TypeInfo rawType = pti.getRawType();
-      StringBuffer sb = new StringBuffer(getDeclaration(ctx, rawType));
-      sb.append('<');
-      List<TypeInfo> arguments = pti.getTypeArguments();
-      for (int i = 0; i < arguments.size(); i++) {
-        TypeInfo argument = arguments.get(i);
-        if (i > 0) {
-          sb.append(',');
-        }
-        sb.append(getDeclaration(ctx, argument));
-      }
-      sb.append('>');
-      return sb.toString();
-    }
-    else if (ti instanceof TypeVariableInfo) {
-      TypeVariableInfo tvi = (TypeVariableInfo)ti;
-      return getDeclaration(ctx, ctx.resolve(tvi));
-    }
-    else if (ti instanceof ArrayTypeInfo) {
-      ArrayTypeInfo ati = (ArrayTypeInfo)ti;
-      return getDeclaration(ctx, ctx.resolve(ati.getComponentType())) + "[]";
-    }
-    else {
-      throw new UnsupportedOperationException("Cannot handle declaration of " + ti);
-    }
-  }
-
-  private String getLiteral(ClassTypeInfo ctx, TypeInfo ti) {
-    if (ti instanceof ClassTypeInfo) {
-      if (ti instanceof VoidTypeInfo) {
-        return "void";
-      }
-      else {
-        return ((ClassTypeInfo)ti).getName();
-      }
-    }
-    else if (ti instanceof ParameterizedTypeInfo) {
-      ParameterizedTypeInfo pti = (ParameterizedTypeInfo)ti;
-      TypeInfo rawType = pti.getRawType();
-      return getDeclaration(ctx, rawType);
-    }
-    else if (ti instanceof TypeVariableInfo) {
-      TypeVariableInfo tvi = (TypeVariableInfo)ti;
-      return getDeclaration(ctx, ctx.resolve(tvi));
-    }
-    else if (ti instanceof ArrayTypeInfo) {
-      ArrayTypeInfo ati = (ArrayTypeInfo)ti;
-      return getDeclaration(ctx, ctx.resolve(ati.getComponentType())) + "[]";
-    }
-    else {
-      throw new UnsupportedOperationException("Cannot handle declaration of " + ti);
-    }
-  }
-
-  private void appendAbstractMethods(String simpleClassName, RoundEnvironment roundEnv, PrintWriter out, ClassTypeInfo cti) {
+  private void appendAbstractMethods(String simpleClassName, RoundEnvironment roundEnv, StringBuilder out, ClassTypeInfo cti) {
 
 
     //
@@ -266,7 +148,6 @@ public class ChromatticProcessor extends AbstractProcessor {
       String methodName = method.getName();
       List<TypeInfo> parameterTypes = method.getParameterTypes();
       TypeInfo rti = method.getReturnType();
-      String returnDeclaration = getDeclaration(cti, rti);
 
       //
       String scope;
@@ -285,43 +166,56 @@ public class ChromatticProcessor extends AbstractProcessor {
       }
 
       //
-      out.write("private static final " + Invoker.class.getSimpleName() + " " + methodId + " = " + Invoker.class.getSimpleName() + ".getDeclaredMethod(" + simpleClassName + ".class,");
-      out.write('"' + methodName + '"');
+      out.append("private static final ").
+          append(Invoker.class.getSimpleName()).
+          append(" ").append(methodId).append(" = ").
+          append(Invoker.class.getSimpleName()).
+          append(".getDeclaredMethod(").
+          append(simpleClassName).
+          append(".class,").
+          append('"').
+          append(methodName).
+          append('"');
       for (TypeInfo parameterType : parameterTypes) {
-        out.write(",");
-        out.write(getLiteral(cti, parameterType));
-        out.write(".class");
+        out.append(",");
+        new TypeFormatter(cti, FormatterStyle.LITERAL, out).format(parameterType);
+        out.append(".class");
       }
-      out.write(");\n");
+      out.append(");\n");
 
       //
-      out.write(scope + " " + returnDeclaration + " " + methodName + "(");
+      out.append(scope).append(" ");
+      new TypeFormatter(cti, FormatterStyle.RETURN_TYPE, out).format(rti);
+      out.append(" ").append(methodName).append("(");
+
+      //
       StringBuffer sb1 = new StringBuffer("Object[] args = new Object[]{");
       for (int i = 0; i < parameterTypes.size(); i++) {
         TypeInfo parameterType = parameterTypes.get(i);
-        String parameterDeclaration = getDeclaration(cti, parameterType);
         if (i > 0) {
-          out.write(",");
+          out.append(",");
           sb1.append(",");
         }
-        out.write(parameterDeclaration + " arg_" + i);
+        new TypeFormatter(cti, FormatterStyle.TYPE_PARAMETER, out).format(parameterType);
+        out.append(" arg_").append(i);
         sb1.append("arg_").append(i);
       }
       sb1.append("};\n");
 
-      out.write(") {\n");
+      out.append(") {\n");
 
-      out.write(sb1.toString());
+      out.append(sb1.toString());
 
       if (rti instanceof VoidTypeInfo) {
-        out.write(methodId + ".invoke(handler, this, args);");
+        out.append(methodId).append(".invoke(handler, this, args);");
       }
       else {
-        out.write("return (" + getCast(cti, rti) + ")" + methodId + ".invoke(handler, this, args);");
+        out.append("return (");
+        new TypeFormatter(cti, FormatterStyle.CAST, out).format(rti);
+        out.append(")").append(methodId).append(".invoke(handler, this, args);");
       }
 
-      out.write("}\n");
-
+      out.append("}\n");
     }
   }
 }
---
+++ apt/src/main/java/org/chromattic/apt/TypeFormatter.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright (C) 2003-2009 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.chromattic.apt;
+
+import org.reflext.api.TypeInfo;
+import org.reflext.api.ClassTypeInfo;
+import org.reflext.api.ParameterizedTypeInfo;
+import org.reflext.api.TypeVariableInfo;
+import org.reflext.api.ArrayTypeInfo;
+import org.reflext.api.WildcardTypeInfo;
+import org.reflext.api.VoidTypeInfo;
+import org.reflext.api.SimpleTypeInfo;
+
+import java.util.List;
+
+/**
+ * @author <a href="mailto:julien.viet@exoplatform.com">Julien Viet</a>
+ * @version $Revision$
+ */
+public class TypeFormatter {
+
+  /** . */
+  private final ClassTypeInfo context;
+
+  /** . */
+  private final FormatterStyle style;
+
+  /** . */
+  private final StringBuilder s;
+
+  public TypeFormatter(ClassTypeInfo context, FormatterStyle style, StringBuilder s) {
+    this.context = context;
+    this.style = style;
+    this.s = s;
+  }
+
+  public void format(TypeInfo ti) {
+    format(ti, false);
+  }
+
+  private void format(TypeInfo ti, boolean fromArray) {
+    if (ti instanceof ClassTypeInfo) {
+      format((ClassTypeInfo)ti, fromArray);
+    } else if (ti instanceof ParameterizedTypeInfo) {
+      format((ParameterizedTypeInfo)ti, fromArray);
+    } else if (ti instanceof TypeVariableInfo) {
+      format((TypeVariableInfo)ti, fromArray);
+    } else if (ti instanceof ArrayTypeInfo) {
+      format((ArrayTypeInfo)ti, fromArray);
+    } else if (ti instanceof WildcardTypeInfo) {
+      format((WildcardTypeInfo)ti, fromArray);
+    } else {
+      throw new UnsupportedOperationException();
+    }
+  }
+
+  private void format(ClassTypeInfo cti, boolean fromArray) {
+    if (cti instanceof VoidTypeInfo) {
+      switch (style) {
+        case CAST:
+        case TYPE_PARAMETER:
+          throw new AssertionError();
+        case RETURN_TYPE:
+        case LITERAL:
+          s.append("void");
+          break;
+      }
+    }
+    else {
+      switch (style) {
+        case CAST:
+          if (!fromArray) {
+            if (cti instanceof SimpleTypeInfo) {
+              switch (((SimpleTypeInfo)cti).getLiteralType()) {
+                case INT:
+                  s.append("java.lang.Integer");
+                  break;
+                case BOOLEAN:
+                  s.append("java.lang.Boolean");
+                  break;
+                case LONG:
+                  s.append("java.lang.Long");
+                  break;
+                case DOUBLE:
+                  s.append("java.lang.Double");
+                  break;
+                case FLOAT:
+                  s.append("java.lang.Float");
+                  break;
+                default:
+                  throw new UnsupportedOperationException();
+              }
+            } else {
+              s.append(cti.getName());
+            }
+          } else {
+            s.append(cti.getName());
+          }
+          break;
+        case LITERAL:
+        case TYPE_PARAMETER:
+        case RETURN_TYPE:
+          s.append(cti.getName());
+          break;
+      }
+    }
+  }
+
+  private void format(ParameterizedTypeInfo pti, boolean fromArray) {
+    TypeInfo rawType = pti.getRawType();
+    format(rawType);
+  }
+
+  private void format(TypeVariableInfo tvi, boolean fromArray) {
+    switch (style) {
+      case LITERAL:
+      case TYPE_PARAMETER:
+      case RETURN_TYPE:
+      case CAST: {
+        TypeInfo resolved = context.resolve(tvi);
+        if (resolved instanceof TypeVariableInfo) {
+          TypeVariableInfo resolvedTVI = (TypeVariableInfo)resolved;
+          List<TypeInfo> bounds = resolvedTVI.getBounds();
+          if (bounds.size() != 1) {
+            throw new UnsupportedOperationException("Need to add support for multiple bounds");
+          }
+          TypeInfo bound = bounds.get(0);
+          format(bound);
+        } else {
+          format(resolved);
+        }
+        break;
+      }
+    }
+  }
+
+  private void format(ArrayTypeInfo ati, boolean fromArray) {
+    switch (style) {
+      case LITERAL:
+      case TYPE_PARAMETER:
+      case RETURN_TYPE:
+      case CAST: {
+        TypeInfo componentTI = ati.getComponentType();
+        format(componentTI, true);
+        s.append("[]");
+        break;
+      }
+    }
+  }
+
+  private void format(WildcardTypeInfo wti, boolean fromArray) {
+    // Do nothing
+  }
+}
